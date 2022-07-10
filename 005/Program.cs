0f27061d89ebdc39b6953655b527b0c92389d897
Denis Rezaykin <drezay@mail.ru>
2022-07-10 22:52:13 +0300

    Task 5

---
+++ 005/Program.cs
@@ -0,0 +1,31 @@
+﻿// С клавиатуры вводятся три числа. Найти максимальное из трех чисел
+int a,b,c;
+System.Console.WriteLine("Введите три числа");
+string? s=Console.ReadLine();
+a=Convert.ToInt32(s);
+s=Console.ReadLine();
+b=Convert.ToInt32(s);
+s=Console.ReadLine();
+c=Convert.ToInt32(s);
+if (a>b)
+{
+    if (a>c)
+    {
+        System.Console.WriteLine("a является максимальным числом");
+    }
+    else
+    {
+        System.Console.WriteLine("c является максимальным числом");
+    }
+}
+else
+{
+ if (b>c)
+    {
+        System.Console.WriteLine("b является максимальным числом");
+    }
+    else
+    {
+        System.Console.WriteLine("c является максимальным числом");
+    }
+}
