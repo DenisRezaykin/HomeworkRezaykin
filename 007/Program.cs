e89bb4f4a1913aca716e316e1953c92db71cde44
Denis Rezaykin <drezay@mail.ru>
2022-07-12 22:30:29 +0300

    Task 7

---
+++ 007/Program.cs
@@ -0,0 +1,12 @@
+﻿int a;
+System.Console.WriteLine("Введите число");
+string? s=System.Console.ReadLine();
+a=Convert.ToInt32(s);
+if (a%2==0)
+{
+    System.Console.WriteLine("Введенное число четное");
+}
+else
+{
+     System.Console.WriteLine("Введенное число нечетное");
+}
