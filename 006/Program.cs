bd9f10e2274309ca5b339d51f1d6cf35f06b3eac
Denis Rezaykin <drezay@mail.ru>
2022-07-13 22:28:02 +0300

    Task 6 м,1,

--- 006/Program.cs
+++ 006/Program.cs
@@ -1,5 +1,17 @@
 ﻿// Написать программу вычисления значения функции y = sin(a). 
+
+/*
 double x,y;
 x=3.1415; //x=Math.PI;
 y=Math.Sin(x);
 System.Console.WriteLine($"sin({x})={y}");
+*/
+
+//тип возвращаемого значения Имя_функции(аргументы функции)
+double Sin(double x) //метод
+{
+    return Math.Sin(x);
+}
+
+double y=Sin(3.14);
+System.Console.WriteLine(y);
