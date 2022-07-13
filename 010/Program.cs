82686b9dfec52653b0c489acb786c40fa8af63cf
Denis Rezaykin <drezay@mail.ru>
2022-07-13 21:05:14 +0300

    Task 10

---
+++ 010/Program.cs
@@ -0,0 +1,8 @@
+﻿// Вывести на экран последнюю цифру целого числа введенного с клавиатуры. 
+int a;
+System.Console.WriteLine("Введите число");
+string? s=System.Console.ReadLine();
+a=Convert.ToInt32(s);
+int c=a%10;
+System.Console.WriteLine(c);
+
