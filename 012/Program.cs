2aee99ff8b18abdbe3e9552d9b5e24167875587c
Denis Rezaykin <drezay@mail.ru>
2022-07-15 01:32:30 +0300

    Task 12

---
+++ 012/Program.cs
@@ -0,0 +1,15 @@
+﻿//С клавиатуры вводится целое число из  диапазона  [10, 99]. Показать наибольшую цифру числа
+int a;
+System.Console.WriteLine("Введите число");
+string? s=System.Console.ReadLine();
+a=Convert.ToInt32(s);
+int b=a%10;
+int c=a/10%10;
+if (b>c)
+{
+    System.Console.WriteLine(b);
+}
+else
+{
+    System.Console.WriteLine(c);
+}
