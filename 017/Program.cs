e6eafdcf92955a8a0c7d722b998d8a49ec4fb400
Denis Rezaykin <drezay@mail.ru>
2022-07-16 19:59:42 +0300

    Task 17

---
+++ 017/Program.cs
@@ -0,0 +1,19 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+System.Console.WriteLine("Введите число обозначающее день недели");
+int a=Convert.ToInt32(Console.ReadLine());
+switch (a)
+{
+    case 6:
+        System.Console.WriteLine(a=6);
+        System.Console.WriteLine("Выходной");
+        break; 
+    case 7:
+        System.Console.WriteLine(a=7);
+        System.Console.WriteLine("Выходной");
+        break;
+    default:
+        System.Console.WriteLine("День недели не является выходным");
+        break;
+}
+
+
