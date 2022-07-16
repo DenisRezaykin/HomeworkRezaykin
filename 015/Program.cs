c8f29145692e9a9a566551c4a92ffbf0d41d5aed
Denis Rezaykin <drezay@mail.ru>
2022-07-16 19:25:54 +0300

    Task 15

---
+++ 015/Program.cs
@@ -0,0 +1,6 @@
+﻿// С клавиатуры вводится целое число. Вывести третью цифру числа или сообщить, что её нет.
+System.Console.WriteLine("Введите число");
+int a=Convert.ToInt32(Console.ReadLine());
+if (a / 100 > 0) System.Console.WriteLine($"Третьей цифрой числа {a} является цифра {a / 100 % 10}");
+else System.Console.WriteLine($"У числа {a} нет третьей цифры");
+
