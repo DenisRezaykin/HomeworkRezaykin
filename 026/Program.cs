5263e70abb164cda8c747829c39d4d67818608d7
Denis Rezaykin <drezay@mail.ru>
2022-07-19 23:26:40 +0300

    Task 26

---
+++ 026/Program.cs
@@ -0,0 +1,9 @@
+﻿// Найти сумму чисел от 1 до А
+
+int N=Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+for (int i = 1; i <= N; i++)
+{
+    sum = sum + i;
+}
+System.Console.WriteLine($" Сумма чисел от 1 до {N} = {sum}");
