3d1b5b805825774371bcc3c48dd9a5308680b979
Denis Rezaykin <drezay@mail.ru>
2022-07-20 22:45:54 +0300

    Task 30

---
+++ 030/Program.cs
@@ -0,0 +1,17 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+System.Console.WriteLine("Введите число N");
+int N=Convert.ToInt32(Console.ReadLine());
+int multiCount(int N)
+{
+    if (N==0) return 1;
+    int k=0, sum = 1;
+    while (N != 0)
+    {
+        k++;
+        sum = sum * (N % 10);
+        N = N / 10;
+    }
+    return sum;
+}
+
+System.Console.WriteLine(multiCount(N));
