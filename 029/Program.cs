02caebda8107f42690a9084f6836d8fa71adf211
Denis Rezaykin <drezay@mail.ru>
2022-07-20 22:34:49 +0300

    Task 29

---
+++ 029/Program.cs
@@ -0,0 +1,14 @@
+﻿// Подсчитать сумму цифр в числе
+int sumCount(int N)
+{
+    int k = 0,
+        sum = 0;
+    while (N != 0)
+    {
+        k++;
+        sum = sum + (N % 10);
+        N = N / 10;
+    }
+    return sum;
+}
+System.Console.WriteLine(sumCount(501));
