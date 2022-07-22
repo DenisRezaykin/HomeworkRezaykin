a6672e0c0deba9c9f6b1581786227aaef2463755
Denis Rezaykin <drezay@mail.ru>
2022-07-22 21:49:48 +0300

    Task 31

---
+++ 031/Program.cs
@@ -0,0 +1,13 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру (взял от 1 до N)
+
+System.Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+int cube = 1;
+for (int i = 1; i <= N; i++)
+{
+    cube = i * i * i;
+    if (cube % 2 == 0)
+    {
+        Console.WriteLine($"{i}^3=" + cube);
+    }
+}
