6046c0a5e4bbdf3d2dd2b3c55fccd4aea884559b
Denis Rezaykin <drezay@mail.ru>
2022-07-23 19:48:33 +0300

    Task 35

---
+++ 035/Program.cs
@@ -0,0 +1,20 @@
+﻿// Написать программу замены элементов массива на противоположные
+
+// Инициализация массива
+int N = 12;
+int[] array = new int[N];
+Random random = new Random(); // псевдослучайные числа
+for (int i = 0; i < array.Length; i++)
+    array[i] = random.Next(-12,10); //Максимальное число надо брать на одно больше, например надо 10, а пишем 11
+
+// Вывод массива до обработки
+for (int i = 0; i < array.Length; i++)
+    System.Console.Write($"{array[i], 4} ");
+    System.Console.WriteLine("");
+
+//Решение задачи
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = array[i] * (-1); // Можно заменить на sumPositive+=array[i]
+    System.Console.Write($"{array[i], 4} ");
+}
