2d1452ead905ed093077905093adfc2d46d9dcda
Denis Rezaykin <drezay@mail.ru>
2022-08-01 22:09:27 +0300

    Task v.1.2

--- 032/Program.cs
+++ 032/Program.cs
@@ -1,9 +1,35 @@
 ﻿// Задать массив из 8 целых элементов и вывести их на экран
-int[] t;
+/*
+int[] t; // объявление
 t = new int[8];
 t[0] = 10;
 t[1] = 12;
+
+// инициализация
 for (int i = 0; i < t.Length; i++)
     t[i] = i * 10;
+//решение задачи
+
+//вывод результатов на экран
 for (int i = 0; i < t.Length; i++)
     System.Console.Write($"a[{i}]={t[i]} ");
+*/
+// Задать массив из 8 целых элементов и вывести их на экран (с использованием подпрограммы)
+
+int[] t;
+Init(out t,8,0,10);
+Print(t, "t");
+
+void Init(out int [] t, int Length, int min, int max)
+{
+    t = new int[8];
+    Random random=new Random();
+    for (int i = 0; i < t.Length; i++)
+    t[i] = random.Next(min,max+1);
+}
+
+void Print (int [] t, string variableName)
+{
+    for (int i = 0; i < t.Length; i++)
+    System.Console.Write($"{variableName}[{i}]={t[i]} ");
+}
