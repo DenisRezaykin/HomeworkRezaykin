40d160f3d05991487248223e9e4075fc80877e3b
Denis Rezaykin <drezay@mail.ru>
2022-08-01 22:51:10 +0300

    Task 34 v.1.2

--- 034/Program.cs
+++ 034/Program.cs
@@ -10,7 +10,7 @@ System.Console.Write($"{array[i],4}"); //4 означает выделение 4
 */
 
 // Рандомный массив
-
+/*
 // Инициализация массива
 int N = 12;
 int[] array = new int[N];
@@ -37,3 +37,51 @@ for (int i = 0; i < array.Length; i++)
 // Вывод результатов
 System.Console.WriteLine(sumPositive);
 System.Console.WriteLine(sumNegative);
+*/
+
+
+// С использованием подпрограмм
+
+// Инициализация массива
+int[] a;
+Init(out a, 12, -9, 9);
+
+// Вывод массива до обработки
+Print(a);
+
+//Решение задачи
+int sumPositive;
+int sumNegative;
+Solve(a, out sumPositive, out sumNegative);
+
+// Вывод результатов
+System.Console.WriteLine(sumPositive);
+System.Console.WriteLine(sumNegative);
+
+// Подпрограммы
+void Init(out int[] t, int Length, int min, int max)
+{
+    t = new int[8];
+    Random random = new Random();
+    for (int i = 0; i < t.Length; i++)
+        t[i] = random.Next(min, max + 1);
+}
+
+void Print(int[] t)
+{
+    for (int i = 0; i < t.Length; i++)
+        System.Console.Write($"{t[i], 4} ");
+}
+
+void Solve(int[] a, out int sumPositive, out int sumNegative)
+{
+    sumPositive = 0;
+    sumNegative = 0;
+    for (int i = 0; i < a.Length; i++)
+        if (a[i] > 0)
+            sumPositive = sumPositive + a[i]; // Можно заменить на sumPositive+=a[i]
+
+    for (int i = 0; i < a.Length; i++)
+        if (a[i] < 0)
+            sumNegative = sumNegative + a[i]; //Можно заменить на sumNegative+=a[i]
+}
