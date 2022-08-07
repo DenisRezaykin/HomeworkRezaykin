45aa54163b5c1f12185240e3e28e62022fe65e25
Denis Rezaykin <drezay@mail.ru>
2022-08-07 20:12:31 +0300

    Task 44

---
+++ 044/Program.cs
@@ -0,0 +1,18 @@
+﻿// Написать программу преобразования десятичного числа в двоичное
+
+int n = 16;
+for (int i = 0; i < 20; i++)
+
+    System.Console.WriteLine($"{i} {DecToBin(i)}");
+string DecToBin(int n)
+{
+    if (n == 0)
+        return "0";
+    string s = "";
+    while (n != 0)
+    {
+        s = (n % 2) + s;
+        n /= 2;
+    }
+    return s;
+}
