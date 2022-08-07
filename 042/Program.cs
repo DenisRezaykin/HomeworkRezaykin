f8382290b47d897b543cbf2ccc2700714d7f663d
Denis Rezaykin <drezay@mail.ru>
2022-08-07 19:51:18 +0300

    Task 42

---
+++ 042/Program.cs
@@ -0,0 +1,10 @@
+﻿// Выяснить являются ли три числа сторонами треугольника
+
+double a=1,b=1,c=10;
+System.Console.WriteLine(IsTreangle(a,b,c));
+
+
+bool IsTreangle(double a, double b, double c)
+{
+    return (a+b>c && b+c>a && a+c>b);
+}
